7cdbe625495aa31d859234c44ae3a1ac820a35e9
Vishv07 <krishnakakadiya9@gmail.com>
2021-07-04 10:33:19 +0530

    Created a Server for Rest Service

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 /*---/ imports /------------------------------------------------------------*/
 
+const express = require("express");
+const app = express();
+const cookieparser = require('cookie-parser');
+var bodyParser = require('body-parser')
+const cors = require('cors');
+const port = process.env.PORT || 3003;
 /* JTF Schema */
 const { JTFSchema, JTFSchema2Str } = require('./Data/JTFSchema.js')
 
@@ -12,6 +18,26 @@ const { importKeiBi } = require('./Loaders/KeiBiLoader.js')
 const { JTFChar2ATFO } = require('./Converters/JTF2ATF_O.js')
 
 const { Create, Read, Update, Delete, Strip } = require('./API/JTFCRUD.js')
+
+//MiddleWares
+app.use(bodyParser.json())
+app.use(cookieparser());
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200
+  }));
+
+app.get('/',(req,res,next)=>{
+    res.json({
+        'jtf-lib': "server-running"
+    })
+})
+
+//Listening on 8081
+app.listen(port, () => {
+    console.log(`app is running on ${port}`);
+});
+
 
 /* JTF to ATF */
 // ToDo. 
