2f51f72bf51873de57e9de58ba0c361d729f4745
Lars Willighagen <lars.willighagen@gmail.com>
2022-02-13 17:20:57 +0100

    Replace request with node-fetch
    Close #8

--- src/Loaders/getHTMLData.js
+++ src/Loaders/getHTMLData.js
@@ -2,21 +2,17 @@
 
 // ? Unused ?
 // Clean-up:
-// * Remove `request`, `request-promise`, `node-html-parser`
-//   if not used elsewere.
+// * Remove `node-html-parser` if not used elsewere.
 
 /*---/ imports /------------------------------------------------------------*/
-const request = require('request');
-const rp = require('request-promise');
+const fetch = require('node-fetch')
 const parseHTML = require('node-html-parser').parse;
 
 const getHTML = async function( url ){
 	// Get the HTML source.
 	// Return `node-html-parser` object.
-	const result = await rp(url);
-	await new Promise (resolve => {
-		setTimeout(resolve, 240000);
-	});
+	const response = await fetch(url);
+	const result = await response.text();
 	return parseHTML(result);
 };
 
